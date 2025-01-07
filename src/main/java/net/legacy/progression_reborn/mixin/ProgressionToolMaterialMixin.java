c8c6af1eb207803f2beda3862ead3d7fd96ea4a6
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-01-07 18:17:44 +0100

    add ProgressionToolMaterialMixin

---
+++ src/main/java/net/legacy/progression_reborn/mixin/ProgressionToolMaterialMixin.java
@@ -0,0 +1,30 @@
+package net.legacy.progression_reborn.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.ToolMaterial;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ToolMaterial.class)
+public interface ProgressionToolMaterialMixin {
+
+    @WrapOperation(
+            method = "<clinit>",
+            at = @At
+                    (value = "NEW",
+                            target = "(Lnet/minecraft/tags/TagKey;IFFILnet/minecraft/tags/TagKey;)Lnet/minecraft/world/item/ToolMaterial;"
+                    )
+    )
+    private static ToolMaterial modifyToughness(TagKey tagKey, int i, float f, float g, int j, TagKey tagKey2, Operation<ToolMaterial> original) {
+        if (tagKey == BlockTags.INCORRECT_FOR_GOLD_TOOL && i == 32 && f == 12f && g == 0f && j == 22 && tagKey2 == ItemTags.GOLD_TOOL_MATERIALS){
+            return original.call(tagKey, 73, 10f, g, j, tagKey2);
+        }
+        else {
+            return original.call(tagKey, i, f, g, j, tagKey2);
+        }
+    }
+}
--- src/main/java/net/legacy/progression_reborn_old/ProgressionTiers.java
+++ src/main/java/net/legacy/progression_reborn_old/ProgressionTiers.java
@@ -13,7 +13,7 @@ import java.util.function.Supplier;
 
 public enum ProgressionTiers implements Tier {
     COPPER(BlockTags.INCORRECT_FOR_STONE_TOOL, 190, 5.0F, 1.0F, 14, () -> Ingredient.of(Items.COPPER_INGOT)),
-    ROSE(BlockTags.INCORRECT_FOR_IRON_TOOL, 141, 12.0F, 2.0F, 22, () -> Ingredient.of(ProgressionItems.ROSE_INGOT));
+    ROSE(BlockTags.INCORRECT_FOR_IRON_TOOL, 141, 10.0F, 2.0F, 22, () -> Ingredient.of(ProgressionItems.ROSE_INGOT));
 
     private final TagKey<Block> incorrectBlocksForDrops;
     private final int uses;
